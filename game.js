f4eea24cf27bffde19395d2c680e896f1b4ab87d
Summerson Gonçalves <summersongoncalves@gmail.com>
2023-01-01 22:32:14 -0300

    added mapp

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+const BORDER_WALL_WIDTH = 1.5
 const canvas = document.getElementById("canvas")
 const canvasContext = canvas.getContext("2d")
 const pacmanFrames = document.getElementById("animation")
@@ -5,42 +6,51 @@ const ghostFrames = document.getElementById("ghosts")
 
 let createRect = (x, y, width, heigth, color) => {
     canvasContext.fillStyle = color
-    canvasContext.fillRect(x, y, width, heigth)
+    canvasContext.fillRect(x, y, width, heigth)    
 }
-
+//30:00
 let fps = 30
 let oneBlockSize = 20
 let wallColor = "#342dCA"
+let wallSpaceWidth = oneBlockSize / BORDER_WALL_WIDTH
+let wallOffset = (oneBlockSize - wallSpaceWidth) / 2
+let wallInnerColor = "#000"
+
+
+//RIGHT  RIGTH
+
+const DIRECTION_RIGHT = 4
+const DIRETCION_UP = 3
+const DIRECTION_LEFT = 2
+const DIRECTION_BOTTOM = 1 
 /**
  * 1 => wall
- * 2 => walk
+ * 0 => walk
  */
-let map = [
-    [1, 1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1,  1],     
-    [1, 2,2,2,2, 2,2,2,2,2, 1,2,2,2,2, 2,2,2,2,2,  1],
-    [1, 2,1,1,1, 2,1,1,1,2, 1,2,1,1,1, 2,1,1,1,2,  1],
-    [1, 2,1,1,1, 2,1,1,1,2, 1,2,1,1,1, 2,1,1,1,2,  1],
-    [1, 2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2,  1],
-    [1, 2,1,1,1, 2,1,2,1,1, 1,1,1,2,1, 2,1,1,1,2,  1],
-    [1, 2,2,2,2, 2,1,2,2,2, 1,2,2,2,1, 2,2,2,2,2,  1],
-    [1, 1,1,1,1, 2,1,1,1,2, 1,2,1,1,1, 2,1,1,1,1,  1],
-    [2, 2,2,2,2, 2,2,2,1,2, 2,2,1,2,2, 2,2,2,2,2,  2],
-
-    [1, 1,1,1,1, 2,1,2,1,2, 2,2,1,2,1, 2,1,1,1,1,  1],
-    [1, 1,1,1,1, 2,1,1,1,1, 1,1,1,1,1, 2,1,1,1,1,  1],
-    [1, 2,1,1,1, 2,1,1,1,1, 1,1,1,1,1, 2,1,1,1,1,  1],
-    [1, 2,1,1,1, 2,1,1,1,1, 1,1,1,1,1, 2,1,1,1,1,  1],
-    [1, 1,1,1,1, 2,1,1,1,1, 1,1,1,1,1, 2,1,1,1,1,  1],
-    [1, 2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2,  1],
-    [1, 2,1,1,1, 2,1,1,1,1, 1,1,1,1,1, 2,1,1,1,1,  1],
-    [1, 2,1,1,1, 2,1,1,1,1, 1,1,1,1,1, 2,1,1,1,1,  1],
-    [1, 1,1,1,1, 2,1,1,1,1, 1,1,1,1,1, 2,1,1,1,1,  1],
-    [1, 2,1,1,1, 2,1,1,1,1, 1,1,1,1,1, 2,1,1,1,1,  1],
-    [1, 2,1,1,1, 2,1,1,1,2, 1,2,1,1,1, 2,1,1,1,2,  1],
-    
-    [1, 2,2,2,2, 2,2,2,2,2, 2,2,2,2,2, 2,2,2,2,2,  1],    
-
-    [ 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1],
+let map = [ 
+    [1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1,1],//23    
+    [1,0,0,0,0, 0,0,0,0,0, 1,0,0,0,0, 0,0,0,0,0,1],//22
+    [1,0,1,1,1, 0,1,1,1,0, 1,0,1,1,1, 0,1,1,1,0,1],//21
+    [1,0,1,1,1, 0,1,1,1,0, 1,0,1,1,1, 0,1,1,1,0,1],//20
+    [1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,1],//19    
+    [1,0,1,1,1, 0,1,0,1,1, 1,1,1,0,1, 0,1,1,1,0,1],//18   
+    [1,0,0,0,0, 0,1,0,0,0, 1,0,0,0,1, 0,0,0,0,0,1],//17
+    [1,1,1,1,1, 0,1,1,1,0, 1,0,1,1,1, 0,1,1,1,1,1],//16
+    [0,0,0,0,1, 0,1,0,0,0, 0,0,0,0,1, 0,1,0,0,0,2],//15
+    [1,1,1,1,1, 0,1,0,1,1, 0,1,1,0,1, 0,1,1,1,1,1],//14
+    [0,0,0,0,0, 0,1,0,1,0, 0,0,1,0,1, 0,0,0,0,0,2],//13
+    [1,1,1,1,1, 0,1,0,1,0, 0,0,1,0,1, 0,1,1,1,1,1],//12
+    [0,0,0,0,1, 0,1,0,1,1, 1,1,1,0,1, 0,1,0,0,0,2],//11
+    [0,0,0,0,1, 0,1,0,0,0, 0,0,0,0,1, 0,1,0,0,0,2],//10
+    [1,1,1,1,1, 0,0,0,1,1, 1,1,1,0,0, 0,1,1,1,1,1],//9
+    [1,0,0,0,0, 0,0,0,0,0, 1,0,0,0,0, 0,0,0,0,0,1],//8    
+    [1,0,1,1,1, 0,1,1,1,0, 1,0,1,1,1, 0,1,1,1,0,1],//7
+    [1,0,0,0,1, 0,0,0,0,0, 0,0,0,0,0, 0,1,0,0,0,1],//6
+    [1,1,0,0,1, 0,1,0,1,1, 1,1,1,0,1, 0,1,0,0,1,1],//5   
+    [1,0,0,0,0, 0,1,0,0,0, 1,0,0,0,1, 0,0,0,0,0,1],//4
+    [1,0,1,1,1, 1,1,1,1,0, 1,0,1,1,1, 1,1,1,1,0,1],//3    
+    [1,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0, 0,0,0,0,0,1],//2 
+    [1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1, 1,1,1,1,1,1],//1
 ]
 
 
@@ -51,19 +61,22 @@ let gameLoop = () => {
 
 let update = () => {
     //TODO    
+    pacman.moveProcess()
 }
 
-let draw = () => {
+let draw = () => {   
+    createRect(0, 0, canvas.width, canvas.height, "#000") // workaround to do not show multiples pacmans
     //TODO
     drawWalls()    
+    pacman.draw()
 }
 
 let gameInterval = setInterval(gameLoop, 1000 / fps); 
 
-let drawWalls = () => {    
-    console.log('desenhando parede')    
+let drawWalls = () => {          
     for( let i = 0; i < map.length; i++) {
         for(let j = 0; j < map[0].length; j++) {
+            // create raw walls
             if(map[i][j] == 1) {
                 createRect(
                     j* oneBlockSize, 
@@ -73,6 +86,52 @@ let drawWalls = () => {
                     wallColor 
                     )
             } 
+
+            if( j > 0 && map[i][ j - 1] == 1) {
+               createRect(
+                j * oneBlockSize, 
+                i * oneBlockSize + wallOffset,
+                wallSpaceWidth + wallOffset,
+                wallSpaceWidth,
+                wallInnerColor) 
+            }
+            if( j < map[0].length -1 && map[i][ j + 1] == 1) {
+               createRect(
+                j * oneBlockSize + wallOffset, 
+                i * oneBlockSize + wallOffset,
+                wallSpaceWidth + wallOffset,
+                wallSpaceWidth,
+                wallInnerColor) 
+            }
+
+             if( i > 0 && map[i - 1][ j ] == 1) {
+                createRect(
+                 j * oneBlockSize + wallOffset, 
+                 i * oneBlockSize ,
+                 wallSpaceWidth,
+                 wallSpaceWidth + wallOffset,
+                 wallInnerColor) 
+             }
+             if( i < map.length - 1 && map[i + 1][ j ] == 1) {
+                createRect(
+                 j * oneBlockSize + wallOffset, 
+                 i * oneBlockSize + wallOffset,
+                 wallSpaceWidth ,
+                 wallSpaceWidth + wallOffset,
+                 wallInnerColor) 
+             } 
         }
     }
 }
+
+let createNewPacman = () => {
+    pacman = new Pacman( 
+        oneBlockSize,
+        oneBlockSize ,
+        oneBlockSize,
+        oneBlockSize,
+        oneBlockSize / 5)
+}
+
+createNewPacman()
+gameLoop()
---
+++ pacman.js
@@ -0,0 +1,131 @@
+class Pacman {
+    constructor(x, y, width, height, speed) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.speed = speed
+        this.direction = DIRECTION_RIGHT
+        this.currentFrame = 1
+        this.frameCount = 7
+        setInterval(() => {
+           this.changeAnimation() 
+        }, 100)
+
+    }
+
+    moveProcess () {
+        this.changeDirectionIfPossible()
+        this.moveFowards()
+        if(this.checkCollision()) {
+            this.moveBackwards()
+        }
+    }
+
+    eat () {
+
+    }
+    moveBackwards () {
+        switch(this.direction) {
+            case DIRECTION_RIGHT: 
+            this.x -= this.speed;
+            break;
+            case DIRETCION_UP: 
+            this.y += this.speed;
+            break;
+            case DIRECTION_LEFT: 
+            this.x += this.speed;
+            break;
+            case DIRECTION_BOTTOM: 
+            this.y -= this.speed;
+            break;
+        }
+    }
+    moveFowards () {
+        switch( this.direction) {
+            case DIRECTION_RIGHT: 
+            this.x += this.speed;
+            break;
+            case DIRETCION_UP: 
+            this.y -= this.speed;
+            break;
+            case DIRECTION_LEFT: 
+            this.x -= this.speed;
+            break;
+            case DIRECTION_BOTTOM: 
+            this.y += this.speed;
+            break;
+        }
+    }
+
+    checkCollision () {       
+        if (
+           map[this.getMapY()][this.getMapX()] == 1
+        || map[this.getMapYRightSide()][this.getMapX()] == 1
+        || map[this.getMapY()][this.getMapXRightSide()] == 1
+        || map[this.getMapYRightSide()][this.getMapXRightSide()] == 1
+        ){
+            return true    
+        }
+        else return false
+
+    }
+
+    checkGhostCollision () {
+
+    }
+    changeDirectionIfPossible () {
+    }
+
+    changeAnimation () {
+        this.currentFrame = this.currentFrame == this.frameCount ? 1 : this.currentFrame + 1
+    }
+
+    /**
+     * draw pacman saving context
+     * and replace in the same origin
+     */
+    draw () {
+        canvasContext.save() // ????
+        canvasContext.translate(
+            this.x + oneBlockSize / 2,  
+            this.y + oneBlockSize / 2
+        )
+        canvasContext.rotate((this.direction * 90 * Math.PI) / 180) // ?????
+        
+        canvasContext.translate(
+            -this.x - oneBlockSize / 2,
+            -this.y - oneBlockSize / 2
+        )
+
+        //// pacmanFrames maybe bug for out of scope game.js
+        //// oneblockSize too ?
+        ////drawImage has multiples constructors
+        canvasContext.drawImage(
+            pacmanFrames,
+            (this.currentFrame - 1) / oneBlockSize, 
+            0, 
+            oneBlockSize,
+            oneBlockSize,
+            this.x,
+            this.y,
+            this.width,
+            this.height
+            );  
+        
+        canvasContext.restore()   // ????     
+    }
+    getMapX () {
+        return parseInt(this.x / oneBlockSize)
+    }
+    getMapY () {
+        return parseInt(this.y / oneBlockSize)
+    }
+    getMapXRightSide () {
+        return parseInt((this.x + 0.9999 * oneBlockSize) / oneBlockSize)
+    }
+    getMapYRightSide () {
+        return parseInt((this.y + 0.9999 * oneBlockSize) / oneBlockSize)
+    }
+
+}
